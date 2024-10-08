535fe8f7bd64a00fe5fe096c3b348aab63c2c24e
PAOLO MICHELOTTI s328846 <s328846@studenti.polito.it>
2024-10-08 18:54:02 +0200

    Added qr in GUI

--- client/src/getTicketComps/getTicketComp.tsx
+++ client/src/getTicketComps/getTicketComp.tsx
@@ -1,14 +1,16 @@
 import { Button, Container } from 'react-bootstrap';
 import { useState } from 'react';
+import QRCode from 'react-qr-code';
 
 export function GetTicketComp() {
 
     const [phase, setPhase] = useState(0);
+    const [ticket, setTicket] = useState('');
 
     // const handleGetTicket = async (service: number) => {
     //     try {
     //       setPhase(1);
-    //       const ticket = await API.getTicket(service);
+    //       const ticket = setTicket(await API.getTicket(service));
     //       setPhase(2);
     //     }catch(err) {
     //       setPhase(0);
@@ -17,20 +19,34 @@ export function GetTicketComp() {
     //   };
 
 
-    return (<>
-        <Container
-            className="d-flex justify-content-center align-items-start" 
-            style={{ height: '100vh', paddingTop: '20px' }}
-        >
-             <div>
-                    <Button variant="primary" style={{ marginBottom: '20px' }} /*onClick={() => handleGetTicket(1)}*/>Service 1</Button>{' '}
-                    <Button variant="primary" style={{ marginBottom: '20px' }} /*onClick={() => handleGetTicket(2)}*/>Service 2</Button>{' '}
-                    <Button variant="primary" style={{ marginBottom: '20px' }} /*onClick={() => handleGetTicket(3)}*/>Service 3</Button>{' '}
-                    <Button variant="primary" style={{ marginBottom: '20px' }} /*onClick={() => handleGetTicket(4)}*/>Service 4</Button>{' '}
-                </div>
-        </Container>
+      return (
+        <>
+          <Container
+      className="d-flex flex-column justify-content-start align-items-center" 
+      style={{ height: '100vh', paddingTop: '20px' }}
+    >
+      <h1>Select a service</h1>
+      <div className="d-flex justify-content-center">
+        <Button variant="primary" style={{ margin: '0 10px' }}>
+          Service 1
+        </Button>
+        <Button variant="primary" style={{ margin: '0 10px' }}>
+          Service 2
+        </Button>
+        <Button variant="primary" style={{ margin: '0 10px' }}>
+          Service 3
+        </Button>
+        <Button variant="primary" style={{ margin: '0 10px' }}>
+          Service 4
+        </Button>
+      </div>
+    </Container>
         {phase === 1 && <h1>Getting ticket...</h1>}
-        {phase === 2 && <h1>Ticket received!</h1>}
+        {phase === 2 && <h1>Ticket received!
+                            <QRCode value={ticket} />
+                        </h1>}
+
     </>
     );
 }
+

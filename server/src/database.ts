6ca7034b0ff6a99784aebe4a17e65058dd780089
Umberto Fontanazza <umberto.fontanazza@gmail.com>
2024-10-16 14:31:04 +0200

    Add db check on shutdown for already closed connection

--- server/src/database.ts
+++ server/src/database.ts
@@ -20,13 +20,14 @@ export class Database {
 
     public static async tryCloseClient(): Promise<void> {
         if (!Database._instance) return
+        if (!Database.instance.connected) return
         await Database.instance.client.end()
     }
 
     private static async checkConnection(): Promise<void> {
         if (Database.instance.connected) return
-        await Database.instance.client.connect()
         Database.instance.connected = true
+        await Database.instance.client.connect()
     }
 
     /**
@@ -42,11 +43,11 @@ export class Database {
 
     public static async assignCounter(counterId: number, serviceNames: string[]): Promise<void> {
         await Database.checkConnection();
-    
+
         const checkSql = `SELECT service_name FROM counter_service WHERE counter_id = $1 AND service_name = $2`;
-        
+
         const insertSql = `INSERT INTO counter_service (counter_id, service_name, date) VALUES ($1, $2, NOW())`;
-    
+
         for (const serviceName of serviceNames) {
             const result = await Database.instance.client.query(checkSql, [counterId, serviceName]);
             if (result.rows.length === 0) {
@@ -57,21 +58,21 @@ export class Database {
 
     // callcustomer
     public static async CallCustomer(customerId: number): Promise<boolean> {
-    await Database.checkConnection();
-    const sql = "INSERT INTO call_log (customer_id, call_time) VALUES ($1, NOW()) returning id;";
-    const { rows } = await Database.instance.client.query(sql, [customerId]);
-    const callLogId = rows.pop()?.id as number;
+        await Database.checkConnection();
+        const sql = "INSERT INTO call_log (customer_id, call_time) VALUES ($1, NOW()) returning id;";
+        const { rows } = await Database.instance.client.query(sql, [customerId]);
+        const callLogId = rows.pop()?.id as number;
 
-    // If callLogId exists, the call log was successfully inserted
-    return callLogId !== undefined;
-}
+        // If callLogId exists, the call log was successfully inserted
+        return callLogId !== undefined;
+    }
 
     public static async getServices(): Promise<any[]> {
         await Database.checkConnection()
         const sql = "SELECT * FROM service"
         const { rows } = await Database.instance.client.query(sql)
         return rows
-    }  
+    }
 
     public static async getActiveServices(): Promise<any[]> {
         await Database.checkConnection();
@@ -80,7 +81,6 @@ export class Database {
             FROM counter_service
         `;
         const { rows } = await Database.instance.client.query(sql);
-        console.log(rows)
         const activeServices = rows.map(row => row.service_name)
         return activeServices;
     }
